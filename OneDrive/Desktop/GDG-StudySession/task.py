dc61e53b6ae3a386d72e8fd81e293666123a98d6
henok dagne <henokdagne19@gmail.com>
2024-12-22 20:21:32 +0300

    Add function to print_even_numbers

--- OneDrive/Desktop/GDG-StudySession/task.py
+++ OneDrive/Desktop/GDG-StudySession/task.py
@@ -4,5 +4,14 @@ def sum_number(num):
         sum += num[i]
     return sum
 
+def even_number():
+    for i in range(0, 20, 2):
+        if (i != 0):
+            print(i, end='')
+
+    print()
+
 numbers = [1, 2, 3, 4, 5]
 print("sum :", sum_number(numbers))
+print("even_number :", end=' ')
+even_number()
